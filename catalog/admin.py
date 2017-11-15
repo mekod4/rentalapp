349c25146472787cfccd773e07a8c67af47742b9
SDQ\emmanuel.jimenez <ejimenez@theoreminc.net>
2017-11-15 13:51:57 -0400

    Site ready for review

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
 from .models import Film, Genre
 
-admin.site.register(Film)
+class FilmAdmin(admin.ModelAdmin):
+	list_display = ('title', 'cost', 'borrower', 'status', 'due_back')
+	list_editable = ('due_back',)
+	list_filter = ('status', 'due_back', )
+
+	def display_genre(self):
+		return ', '.join([ genre.name for genre in self.genre.all()[:3] ])
+
+
+admin.site.register(Film, FilmAdmin)
 admin.site.register(Genre)
